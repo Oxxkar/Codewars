a5bc9c6fb4b8b8696868692224249a96fb1cd951
Oxxkar <osc94lop@gmail.com>
2019-03-07 15:35:06 -0600

    Create watermelon.py

---
+++ watermelon.py
@@ -0,0 +1,5 @@
+# marzo 07, 2019 
+def divide(weight):
+     return weight > 2 and weight & 1 == 0 # Respuesta googleada
+    # return weight % 2 == 0 # Esta solucion da error cuando weight es 2. 2 = 1 + 1 el numero uno es numero impar
+	

5583daeda205d43aae450aff70c6308f0f5f4c0e
Oxxkar <osc94lop@gmail.com>
2019-03-08 11:34:32 -0600

    Add files via upload

---
+++ BasicMathOperations.py
@@ -0,0 +1,13 @@
+# marzo 08, 2019 
+
+def basic_op(operator, value1, value2):
+	if operator == "+":
+		return value1 + value2
+	if operator == "-":
+		return value1 - value2
+	if operator == "/":
+		return value1 / value2
+	if operator == "*":
+		return value1 * value2
+
+
